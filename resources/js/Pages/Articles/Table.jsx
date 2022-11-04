dec21e5ef2235b304a2033bf2b35348c356a9cdb
ryanhasbie <ryanhasbie2001@gmail.com>
2022-11-04 23:20:36 +0700

    create components table

--- resources/js/Pages/Articles/Table.jsx
+++ resources/js/Pages/Articles/Table.jsx
@@ -3,36 +3,47 @@ import App from '@/Layouts/App'
 import Container from '@/Components/Container'
 import { Link } from '@inertiajs/inertia-react'
 import Pagination from '@/Components/Pagination'
+import Table from '@/Components/Table'
 
 export default function ArticleTable(props) {
     const {data: articles, meta, links} = props.articles
     return (
         <Container>
-            <table>
-                <thead>
+            <Table>
+                <Table.Thead>
                     <tr>
-                        <th>#</th>
-                        <th>Title</th>
-                        <th>Category</th>
-                        <th>Tags</th>
+                        <Table.Th>#</Table.Th>
+                        <Table.Th>Title</Table.Th>
+                        <Table.Th>Category</Table.Th>
+                        <Table.Th>Tags</Table.Th>
+                        <th></th>
                     </tr>
-                </thead>
-                <tbody>
+                </Table.Thead>
+                <Table.Tbody>
                     {articles.length ?
                         articles.map((article, i) => (
                             <tr key={article.id}>
-                                <td>{meta.from + i}</td>
-                                <td><Link href={article.url}>{article.title}</Link></td>
-                                <td><Link href={article.category.url}>{article.category.name}</Link></td>
-                                {article.tags.map((tag, i) => (
-                                    <td><Link key={i} href={tag.url}>{tag.name}</Link></td>
-                                ))}
+                                <Table.Td>{meta.from + i}</Table.Td>
+                                <Table.Td><Link href={article.url}>{article.title}</Link></Table.Td>
+                                <Table.Td><Link href={article.category.url}>{article.category.name}</Link></Table.Td>
+                                <Table.Td>
+                                    {article.tags.map((tag, i) => (
+                                        <Link key={i} href={tag.url}>{tag.name}</Link>
+                                    ))}
+                                </Table.Td>
+                                <td>
+                                    <Table.Dropdown>
+                                        <Table.DropdownItem className='hover:bg-green-50 hover:text-green-500'href={'#'}>View</Table.DropdownItem>
+                                        <Table.DropdownItem className='hover:bg-yellow-50 hover:text-yellow-500' href={'#'}>Edit</Table.DropdownItem>
+                                        <Table.DropdownItem className='hover:bg-rose-50 hover:text-rose-500' href={'#'}>Delete</Table.DropdownItem>
+                                    </Table.Dropdown>
+                                </td>
                             </tr>
                         ))
                     : <p>No articles yet.</p>
                     }
-                </tbody>
-            </table>
+                </Table.Tbody>
+            </Table>
             <Pagination {...{meta, links}}/>
         </Container>
     )
